44fce8efc2314819be007a8ebbb8b7abb01f254c
charsskim <mcprince73@naver.com>
2023-12-09 13:14:45 +0900

    버튼클릭했을때 위치 이동과 색상

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,31 @@
+import { Link, NavLink } from 'react-router-dom';
 export default function Header() {
 	return (
 		<div className='Header'>
-			<h1>DCODELAB</h1>
+			<h1>
+				<Link to='/'>DCODELAB</Link>
+			</h1>
 			<ul>
-				<li>Department</li>
-				<li>Youtube</li>
-				<li>Gallery</li>
-				<li>Location</li>
+				<li>
+					<NavLink to='/department' activeClassName={'on'}>
+						Department
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/youtube' activeClassName={'on'}>
+						Youtube
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/gallery' activeClassName={'on'}>
+						Gallery
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to='/location' activeClassName={'on'}>
+						Location
+					</NavLink>
+				</li>
 			</ul>
 		</div>
 	);
